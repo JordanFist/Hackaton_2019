a2ea6f181c96e45955d44c7db40164a1827c77a7
ijosse001 <ijosse001@enseirb-matmeca.fr>
2019-03-23 19:36:31 +0100

    add class point java

---
+++ src/SafeArea.java
@@ -0,0 +1,13 @@
+
+
+class SafeArea extends AbstractPointOfInterest{
+
+  /*
+  *   compare 2 point
+  *   arguments:
+  *   return:
+  */
+  int cmp_pointofinterest(SafeArea p1, SafeArea p2){
+
+  }
+}
